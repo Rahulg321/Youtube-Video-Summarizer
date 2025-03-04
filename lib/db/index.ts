4adad57ca3fc2af117f89131be465129cca425e3
Rahul Gupta <rg5353070@gmail.com>
2025-03-04 13:45:06 +0530

    Initial commit

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,6 +2,11 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { env } from "@/lib/env.mjs";
 
+/**
+ * DOCKER COMMAND WITH PG-VECTOR TO START A POSTGRES DATABASE WITH EXTENSION INSTALLED docker run -d --name pgvector_db -p 6500:5432 -e POSTGRES_PASSWORD=mysecretpassword pgvector/pgvector:pg16
+ *
+ *
+ * */
+
 const client = postgres(env.DATABASE_URL);
 export const db = drizzle(client);
-
